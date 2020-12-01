5ad937a937d192527027ba9b6f64883c5a67a242
a9jiwani <a9jiwani@student.cs.uwaterloo.ca>
2020-11-30 20:02:18 -0500

    added card class files

---
+++ card.h
@@ -0,0 +1,27 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+#include <memory>
+#include <stack>
+#include "player.h"
+
+// may need helper funciton to move from deck to hand to board to etc.
+
+class Card  {
+	std::string name;
+	std::string type;
+	std::string description;
+	int cost;
+	Player owner;
+
+	public:
+		Card(const std::string &name); 
+		std::string getName() const;
+		std::string getType() const;
+		std::string getDescription() const;
+		int getCost() const;
+		Player getOwner() const;
+};
+
+#endif
