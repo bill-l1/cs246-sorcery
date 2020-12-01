e421b255e07460a5fffcd1a18dc906a7667058f6
a9jiwani <a9jiwani@student.cs.uwaterloo.ca>
2020-11-30 19:34:41 -0500

    Added deck load feature (needs card class to work, may have to come back to it

--- player.h
+++ player.h
@@ -11,7 +11,7 @@ class Player  {
 	std::string name;
 	int life;
 	int magic;
-	//std::stack<std::unique_ptr<Card>> deck;
+	std::stack<std::unique_ptr<Card>> deck;
 	//std::vector<std::unique_ptr<Card>> hand;
 	//std::vector<std::unique_ptr<Card>> board;
 	//std::stack<std::unique_ptr<Card>> graveyard;
@@ -22,6 +22,7 @@ class Player  {
 		std::string getName() const;
 		int getLife() const;
 		int getMagic() const;
+		void loadDeck(); //load deck from file, we kinda need this
 		//void runOnPlay(); //iterate through all board/rituals for onPlay effects
 		//void runOnStartTurn(); 
 		//void runOnEndTurn();
