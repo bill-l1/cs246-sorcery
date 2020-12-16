f78ec22bd9009cc3eb0ab5a81955f7f624b01941
a9jiwani <a9jiwani@student.cs.uwaterloo.ca>
2020-12-16 15:47:13 -0500

    made spells exception safe, todo fix unsummon

--- handeffect.cc
+++ handeffect.cc
@@ -3,15 +3,16 @@
 #include "effect.h"
 #include "handeffect.h"
 #include "ritual.h"
-HandEffect::HandEffect(Player * own, Card * tar) :
+HandEffect::HandEffect(Player * own, Card * tar, std::unique_ptr<Minion> mtar) :
 	Effect{own, tar}
+	bref{mtar}
 {}
 
 void HandEffect::run() {
 	BaseMinion * m = dynamic_cast<BaseMinion *>(getTarget());
 	if( m != nullptr) {
 		getGame()->printAlert(m->getMinionName() + " returned to " + m->getOwner()->getName() + "'s hand!", 1);
-		m->getOwner()->addToHand(m);
+		m->getOwner()->addToHand(mtar);
 		m->getOwner()->removeFromBoard(m);	
 	}
 }
